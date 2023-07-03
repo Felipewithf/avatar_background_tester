9d6fc21d83bbd563e207ca976de873e7feb39065
Felipe <felipe.sarmiento.as@gmail.com>
2023-07-03 12:29:11 -0400

    added change of avatars with AWSD keypressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,60 +16,176 @@ var background = [{
 },
 ];
 
+var avatar = [{
+    "name": "something",
+    "code": "a1",
+    "num": [1,2]
+},
+{
+    "name": "something",
+    "code": "a2",
+    "num": [1]
+},
+{
+    "name": "something",
+    "code": "a3",
+    "num": [1]
+},
+{
+    "name": "something",
+    "code": "a4",
+    "num": [1]
+},
+{
+    "name": "something",
+    "code": "a5",
+    "num": [1]
+},
+
+];
+
 var variationIndex = 0; //left to right
 var categoryIndex = 0; //up and down
 
-console.log(variationIndex);
+var variationIndexAvatar = 0; //left to right (Avatars)
+var categoryIndexAvatar = 0; //up and down (Avatars)
 
-function changeStageBackground(event) {
+
+
+// render the thumbnails of background
+for (const backgroundsThumbnails of background) {
+    var bck_thumbnailSrc = `./assets/img/bg/${backgroundsThumbnails.code}/1.jpg` ;
+    $(".bgItems").append($("<li>")
+                    .append($("<img>").attr("src",bck_thumbnailSrc) 
+                    ));
+};
+
+// render the thumbnails of avatars
+for (const avatarsThumbnails of avatar) {
+    var avt_thumbnailSrc = `./assets/img/av/${avatarsThumbnails.code}/1.png` ;
+    $(".avItems").append($("<li>")
+                    .append($("<img>").attr("src",avt_thumbnailSrc) 
+                    ));
+};
+
+function changeAssets(event){
     event.preventDefault();
     console.log(event.keyCode);
+    //check if is a background change
+    if(event.keyCode > 36 && event.keyCode < 41 ){
+        changeStageBackground(event);
+    };
+    //check if is an avatar change
+    if(event.keyCode == 65 || event.keyCode == 68 || event.keyCode == 83 || event.keyCode == 87 ){
+        changeStageAvatar(event);
+    };
+}
+
+function changeStageAvatar(event) {
+
+    if (event.keyCode == 68) //right 
+    {
+        variationIndexAvatar++;
+        $('#avatarStage').attr("src", `./assets/img/av/${givemeVariationIndex("a")}.png`)
+
+    };
+    if (event.keyCode == 65) //left
+    {
+        variationIndexAvatar--;
+        $('#avatarStage').attr("src", `./assets/img/av/${givemeVariationIndex("a")}.png`)
+
+    };
+    if (event.keyCode == 87) //Up
+    {
+        categoryIndexAvatar++;
+        $('#avatarStage').attr("src", `./assets/img/av/${givemeCategoryIndex("a")}.png`)
+    };
+    if (event.keyCode == 83) //Down 
+    {
+        categoryIndexAvatar--;
+        $('#avatarStage').attr("src", `./assets/img/av/${givemeCategoryIndex("a")}.png`)
+    };
+   
+};
+
+function changeStageBackground(event) {
+
     if (event.keyCode == 39) //right 
     {
         variationIndex++;
-        $('#stage').attr("src", `./assets/img/bg/${givemeVariationIndex()}.jpg`)
+        $('#stage').attr("src", `./assets/img/bg/${givemeVariationIndex("b")}.jpg`)
 
     };
     if (event.keyCode == 37) //left
     {
         variationIndex--;
-        $('#stage').attr("src", `./assets/img/bg/${givemeVariationIndex()}.jpg`)
+        $('#stage').attr("src", `./assets/img/bg/${givemeVariationIndex("b")}.jpg`)
 
     };
     if (event.keyCode == 38) //top 
     {
         categoryIndex++;
-        $('#stage').attr("src", `./assets/img/bg/${givemeCategoryIndex()}.jpg`)
-        console.log(categoryIndex);
+        $('#stage').attr("src", `./assets/img/bg/${givemeCategoryIndex("b")}.jpg`)
     };
     if (event.keyCode == 40) //bottom 
     {
         categoryIndex--;
-        $('#stage').attr("src", `./assets/img/bg/${givemeCategoryIndex()}.jpg`)
-       console.log(categoryIndex);
+        $('#stage').attr("src", `./assets/img/bg/${givemeCategoryIndex("b")}.jpg`)
     };
    
 };
 
-function givemeVariationIndex(){
-    //check if there are no more variations of backgrounds
-    if(variationIndex == background[categoryIndex].num.length){
-        variationIndex = 0;
-    }else if(variationIndex == -1){
-        variationIndex = background[categoryIndex].num.length -1;
+function givemeVariationIndex(b_a){
+    // check if is a background or avatar
+    if(b_a === "b"){
+        //check if there are no more variations of backgrounds
+        if(variationIndex == background[categoryIndex].num.length){
+            variationIndex = 0;
+        }else if(variationIndex == -1){
+            variationIndex = background[categoryIndex].num.length -1;
+        }
+        return (`${background[categoryIndex].code}/${background[categoryIndex].num[variationIndex]}`);
+
+    }else {
+        //else assume is an avatar change
+        console.log(variationIndexAvatar);
+        //check if there are no more variations of \avatars
+        if(variationIndexAvatar == avatar[categoryIndexAvatar].num.length){
+            variationIndexAvatar = 0;
+            
+        }else if(variationIndexAvatar == -1){
+            variationIndexAvatar = avatar[categoryIndexAvatar].num.length -1;
+        }
+        console.log(variationIndexAvatar);
+        return (`${avatar[categoryIndexAvatar].code}/${avatar[categoryIndexAvatar].num[variationIndexAvatar]}`);
     }
-    return (`${background[categoryIndex].code}/${background[categoryIndex].num[variationIndex]}`);
 }
 
-function givemeCategoryIndex(){
-    //check if there are no more categories of backgrounds
-    if(categoryIndex == background.length){
-      categoryIndex = 0;
-      }else if(categoryIndex == -1){
-         categoryIndex = background.length -1;
-     }
-    return (`${background[categoryIndex].code}/${background[categoryIndex].num[0]}`);
+function givemeCategoryIndex(b_a){
+    
+    // check if is a background or avatar
+    if(b_a === "b"){
+        //check if there are no more categories of backgrounds
+        if(categoryIndex == background.length){
+        categoryIndex = 0;
+        }else if(categoryIndex == -1){
+            categoryIndex = background.length -1;
+        }
+        return (`${background[categoryIndex].code}/${background[categoryIndex].num[0]}`);
+    }else {
+        //else assume is an avatar change
+        if(categoryIndexAvatar == avatar.length){
+            categoryIndexAvatar = 0;
+            }else if(categoryIndexAvatar == -1){
+                categoryIndexAvatar = avatar.length -1;
+            }
+            return (`${avatar[categoryIndexAvatar].code}/${avatar[categoryIndexAvatar].num[0]}`);
+
+    }
 }
 
-$(document).keydown(changeStageBackground);
+
+
+$(document).keydown(changeAssets);
+
 
